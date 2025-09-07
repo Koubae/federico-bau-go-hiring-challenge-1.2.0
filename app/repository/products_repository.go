2bc21099d485240d09c3b6d381a3afb566a0c49e
koubae <58447627+Koubae@users.noreply.github.com>
2025-09-07 14:21:32 +0200

    Added product count in cache

--- app/repository/products_repository.go
+++ app/repository/products_repository.go
@@ -1,17 +1,33 @@
 package repository
 
 import (
+	"log"
+	"time"
+
+	"github.com/jellydator/ttlcache/v3"
 	"github.com/mytheresa/go-hiring-challenge/app/database"
 	"github.com/mytheresa/go-hiring-challenge/app/models"
 )
 
+const (
+	CacheTTL             = 3 * time.Minute
+	CacheProductCountKey = "products__count"
+)
+
 type SQLProductsRepository struct {
-	db *database.Client
+	db    *database.Client
+	cache *ttlcache.Cache[string, *int64] // cache key to count
 }
 
 func NewProductsRepository(db *database.Client) *SQLProductsRepository {
+	cache := ttlcache.New[string, *int64](
+		ttlcache.WithTTL[string, *int64](CacheTTL),
+	)
+	go cache.Start()
+
 	return &SQLProductsRepository{
-		db: db,
+		db:    db,
+		cache: cache,
 	}
 }
 
@@ -31,9 +47,16 @@ func (r *SQLProductsRepository) GetAllProductsWithPagination(limit int, offset i
 }
 
 func (r *SQLProductsRepository) Count() (*int64, error) {
+	if item := r.cache.Get(CacheProductCountKey); item != nil {
+		return item.Value(), nil
+	}
+	log.Println("No product count in cache, fetching from database")
+
 	var count int64
 	if err := r.db.DB.Model(&models.Product{}).Count(&count).Error; err != nil {
 		return nil, err
 	}
+
+	r.cache.Set(CacheProductCountKey, &count, CacheTTL)
 	return &count, nil
 }
