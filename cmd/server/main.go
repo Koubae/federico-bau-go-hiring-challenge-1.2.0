37cd1aa1ee3056454097eced13517643eef1d43b
koubae <58447627+Koubae@users.noreply.github.com>
2025-09-07 17:46:04 +0200

    Refactor Main into app.server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,67 +1,7 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/joho/godotenv"
-	"github.com/mytheresa/go-hiring-challenge/app/catalog"
-	"github.com/mytheresa/go-hiring-challenge/app/category"
-	"github.com/mytheresa/go-hiring-challenge/app/container"
-	"github.com/mytheresa/go-hiring-challenge/app/middlewares"
-)
+import "github.com/mytheresa/go-hiring-challenge/app"
 
 func main() {
-	// Load environment variables from .env file
-	if err := godotenv.Load(".env"); err != nil {
-		log.Fatalf("Error loading .env file: %s", err)
-	}
-
-	// signal handling for graceful shutdown
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-
-	container.CreateDIContainer()
-	defer container.ShutDown()
-
-	// Set up routing
-	mux := http.NewServeMux()
-
-	categoryHandler := category.NewCategoryHandler(container.Container.CategoryRepository)
-	mux.HandleFunc("GET /categories", categoryHandler.ListCategories)
-	mux.HandleFunc("POST /categories", categoryHandler.CreateCategory)
-
-	catalogHandler := catalog.NewCatalogHandler(container.Container.ProductRepository)
-	mux.HandleFunc("GET /catalog", catalogHandler.ListCatalog)
-	mux.HandleFunc("GET /catalog/{code}", catalogHandler.GetProductDetails)
-
-	var handler http.Handler = mux
-	handler = middlewares.RecoverPanic(middlewares.LogAccessMiddleware(mux))
-
-	// Set up the HTTP server
-	// TODO: better way to handle how we load env variables!"
-	srv := &http.Server{
-		Addr:    fmt.Sprintf("localhost:%s", os.Getenv("HTTP_PORT")),
-		Handler: handler,
-	}
-
-	// Start the server
-	go func() {
-		log.Printf("Starting server on http://%s", srv.Addr)
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("Server failed: %s", err)
-		}
-
-		log.Println("Server stopped gracefully")
-	}()
-
-	<-ctx.Done()
-	log.Println("Shutting down server...")
-	srv.Shutdown(ctx)
-	stop()
+	app.RunServer()
 }
